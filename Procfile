35e59cd43c919d6f9f132a7a4fe78caa9e80cf49
Abhay <Abhaycbr07@gmail.com>
2023-08-08 15:15:23 +0530

    commit for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn nerdy.wsgi:application --log-file -
--- nerdy/settings.py
+++ nerdy/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-# import os
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,24 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'  # Static files URL
-STATICFILES_DIRS = [BASE_DIR / "static"]  # Static files directory in the root
-
-# Additional locations of static files
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'app/static'),
-#     # Add more paths as needed
-# ]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+STATIC_URL = 'static/'  # Static files URL
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# settings.py
-
-# ...
 
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'Static'),)
 
-# ...
+django_heroku.settings(locals())
+
+
 
