69bea8ea3faee41e6abcb984939f1474d0535afa
Abhay <Abhaycbr07@gmail.com>
2023-08-08 15:52:22 +0530

    commit

--- nerdy/settings.py
+++ nerdy/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'  # Static files URL
+STATIC_URL = '/static/'  # Static files URL
 STATICFILES_DIRS = [BASE_DIR / "static"]  # Static files directory in the root
 
 # Additional locations of static files
